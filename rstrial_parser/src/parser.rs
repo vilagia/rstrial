0847103e7bdf14f85f47694de87ac7903d6779f8
vilagia <type99hsp@gmail.com>
2023-07-31 01:58:13 +0900

    Can determine exclamation

--- rstrial_parser/src/parser.rs
+++ rstrial_parser/src/parser.rs
@@ -1,3 +1,4 @@
 pub mod line_parser;
 pub mod richtext_parser;
 pub mod section_parser;
+mod terminator_parser;
---
+++ rstrial_parser/src/parser/terminator_parser.rs
@@ -0,0 +1,56 @@
+use std::str::Chars;
+
+use crate::tokens::{
+    line_item::{Attribute, Terminator},
+    LineItem,
+};
+
+pub struct TerminatorParser {
+    pub source: String,
+    state: State,
+}
+
+enum State {
+    Normal,
+}
+
+impl TerminatorParser {
+    pub fn new(text: &str) -> Self {
+        Self {
+            source: text.to_string(),
+            state: State::Normal,
+        }
+    }
+
+    pub fn parse(&self) -> Terminator {
+        let terminator: String = self.source.clone();
+        let exclamations = vec!["!", "?", "！", "？"];
+        let is_exclamation_suffixed = exclamations
+            .into_iter()
+            .any(|exclamation| terminator.ends_with(exclamation));
+
+        match is_exclamation_suffixed {
+            true => Terminator::Exclamation(terminator),
+            false => Terminator::Normal(terminator),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_terminator_exclamation() {
+        let mut parser = TerminatorParser::new("！！。！？！！");
+        let token = parser.parse();
+        assert_eq!(token, Terminator::Exclamation("！！。！？！！".to_string()));
+    }
+
+    #[test]
+    fn test_parse_terminator_normal() {
+        let mut parser = TerminatorParser::new("！！。！？！」");
+        let token = parser.parse();
+        assert_eq!(token, Terminator::Normal("！！。！？！」".to_string()));
+    }
+}
