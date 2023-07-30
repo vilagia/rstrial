00637e37fccf2e6444eb25abbe1ab699b1fa8900
vilagia <type99hsp@gmail.com>
2023-07-30 10:15:59 +0900

    先頭にルビが来るケースに対応

--- rstrial_parser/src/parser/line_parser.rs
+++ rstrial_parser/src/parser/line_parser.rs
@@ -35,42 +35,46 @@ impl Iterator for LineParser<'_> {
             return Some(token);
         }
         let mut token: Option<LineItem> = None;
-                let mut texts = vec![];
-                for char in self.chars.by_ref() {
+        let mut texts = vec![];
+        for char in self.chars.by_ref() {
             match &self.state {
                 State::Normal => match char {
-                        '。' | '！' | '？' | '」' => {
-                            self.stacked_tokens
-                                .push(LineItem::EndOfSentence(char.to_string()));
+                    '。' | '！' | '？' | '」' => {
+                        self.stacked_tokens
+                            .push(LineItem::EndOfSentence(char.to_string()));
+                        token = Some(LineItem::Text(texts.concat()));
+                        break;
+                    }
+                    '、' | ',' => {
+                        self.stacked_tokens.push(LineItem::Comma(char.to_string()));
+                        token = Some(LineItem::Text(texts.concat()));
+                        break;
+                    }
+                    '{' => {
+                        self.state = State::Brace;
+                        if texts.len() > 0 {
                             token = Some(LineItem::Text(texts.concat()));
-                            break;
-                        }
-                        '、' | ',' => {
-                            self.stacked_tokens.push(LineItem::Comma(char.to_string()));
-                            token = Some(LineItem::Text(texts.concat()));
-                            break;
-                        }
-                        '{' => {
-                            self.state = State::Brace;
+                        } else {
                             token = self.next();
-                            break;
                         }
-                        _ => {
-                            texts.push(char.to_string());
-                        }
+                        break;
+                    }
+                    _ => {
+                        texts.push(char.to_string());
+                    }
                 },
                 State::Brace => match char {
-                        '}' => {
-                            self.state = State::Normal;
-                            let rich_text: String = texts.concat();
-                            let mut richtext_parser =
+                    '}' => {
+                        self.state = State::Normal;
+                        let rich_text: String = texts.concat();
+                        let mut richtext_parser =
                             crate::parser::richtext_parser::RichTextParser::new(rich_text.as_str());
-                            token = Some(richtext_parser.parse());
-                            break;
-                        }
-                        _ => {
-                            texts.push(char.to_string());
-                        }
+                        token = Some(richtext_parser.parse());
+                        break;
+                    }
+                    _ => {
+                        texts.push(char.to_string());
+                    }
                 },
             }
         }
@@ -104,6 +108,31 @@ mod tests {
                 LineItem::EndOfSentence("。".to_string()),
             ];
             let parser = LineParser::new("我が輩は、{猫|ねこ}である。名前は、まだ無い。");
+            let actual = parser.collect::<Vec<LineItem>>();
+            assert_eq!(actual, expected);
+        }
+        #[test]
+        fn it_returns_text_token_ruby_on_head() {
+            let expected = vec![
+                LineItem::RichText(
+                    "吾輩".to_string(),
+                    tokens::line_item::Attribute::Ruby("わがはい".to_string()),
+                ),
+                LineItem::Text("は猫である".to_string()),
+                LineItem::EndOfSentence("。".to_string()),
+                LineItem::Text("名前はまだ無い".to_string()),
+                LineItem::EndOfSentence("。".to_string()),
+                LineItem::Text("どこで生れたかとんと".to_string()),
+                LineItem::RichText(
+                    "見当".to_string(),
+                    tokens::line_item::Attribute::Ruby("けんとう".to_string()),
+                ),
+                LineItem::Text("がつかぬ".to_string()),
+                LineItem::EndOfSentence("。".to_string()),
+            ];
+            let parser = LineParser::new(
+                "{吾輩|わがはい}は猫である。名前はまだ無い。どこで生れたかとんと{見当|けんとう}がつかぬ。",
+            );
             let actual = parser.collect::<Vec<LineItem>>();
             assert_eq!(actual, expected);
         }
