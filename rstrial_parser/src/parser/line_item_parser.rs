a6aa1c9cf2e02f0beae8e89709e5861a5daa2c2b
vilagia <type99hsp@gmail.com>
2023-08-01 03:01:08 +0900

    move parser

---
+++ rstrial_parser/src/parser/line_item_parser.rs
@@ -0,0 +1,29 @@
+use logos::Lexer;
+
+use crate::tokens::{LineItem, line_item::{Terminator, Attribute}};
+
+use super::terminator_parser::TerminatorParser;
+
+pub struct LineItemParser;
+
+impl LineItemParser {
+    pub fn to_string(lex: &mut Lexer<LineItem>) -> String {
+        lex.slice().to_owned()
+    }
+    
+    pub fn to_terminator(lex: &mut Lexer<LineItem>) -> Terminator {
+        let slice = lex.slice().to_string();
+        let parser = TerminatorParser::new(slice.as_str());
+        parser.parse()
+    }
+    
+    pub fn to_rich_text(lex: &mut Lexer<LineItem>) -> Option<(String, Attribute)> {
+        let slice = lex.slice().to_string();
+        let (text, attr) = slice
+            .strip_prefix('{')?
+            .strip_suffix('}')?
+            .split_once('|')
+            .map(|(a, b)| (a.to_string(), b.to_string()))?;
+        Some((text, Attribute::Ruby(attr)))
+    }
+}
