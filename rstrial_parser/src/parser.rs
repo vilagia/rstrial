af048c177ca418aa844347a93b09417e9f1cf14d
vilagia <type99hsp@gmail.com>
2023-08-01 03:01:08 +0900

    move parser

--- rstrial_parser/src/parser.rs
+++ rstrial_parser/src/parser.rs
@@ -1,2 +1,3 @@
 pub(crate) mod section_parser;
 pub(crate) mod terminator_parser;
+pub(crate) mod line_item_parser;
--- rstrial_parser/src/tokens/line_item.rs
+++ rstrial_parser/src/tokens/line_item.rs
@@ -1,47 +1,27 @@
-use logos::{Lexer, Logos};
+use logos::Logos;
 
-use crate::parser::terminator_parser::TerminatorParser;
+use crate::parser::line_item_parser::LineItemParser;
 
 // Tokens for novel-style text.
 #[derive(Logos, Debug, PartialEq, Clone)]
 pub enum LineItem {
     // Plaintext to be rendered as-is.
-    #[regex(r"[^!?！？。」]+", priority = 0, callback = parse_to_string)]
+    #[regex(r"[^!?！？。」]+", priority = 0, callback = LineItemParser::to_string)]
     Text(String),
     // A Sentence delimiter such as `,` or `、`.
-    #[regex(r",|、|，", parse_to_string)]
+    #[regex(r",|、|，", LineItemParser::to_string)]
     Comma(String),
     // A comment that should be ignored.
-    #[regex(r"\{#\w+\}", parse_to_string)]
+    #[regex(r"\{#\w+\}", LineItemParser::to_string)]
     Comment(String),
     // Text to be rendered with additional styles.
-    #[regex(r"\{\w+\|\w+\}", parse_rich_text)]
+    #[regex(r"\{\w+\|\w+\}", LineItemParser::to_rich_text)]
     RichText((String, Attribute)),
     // End of sentence. Includes a string shows the end of sentence(e.g. `.`, `。` or `！`).
-    #[regex(r"[!?！？。」]+", priority = 2, callback = parse_terminator)]
+    #[regex(r"[!?！？。」]+", priority = 2, callback = LineItemParser::to_terminator)]
     EndOfSentence(Terminator),
     // End of section such as a scene or a chapter. Includes a string shows the end of section(e.g. `†`).
     EndOfSection(String),
-}
-
-fn parse_to_string(lex: &mut Lexer<LineItem>) -> String {
-    lex.slice().to_owned()
-}
-
-fn parse_terminator(lex: &mut Lexer<LineItem>) -> Terminator {
-    let slice = lex.slice().to_string();
-    let parser = TerminatorParser::new(slice.as_str());
-    parser.parse()
-}
-
-fn parse_rich_text(lex: &mut Lexer<LineItem>) -> Option<(String, Attribute)> {
-    let slice = lex.slice().to_string();
-    let (text, attr) = slice
-        .strip_prefix('{')?
-        .strip_suffix('}')?
-        .split_once('|')
-        .map(|(a, b)| (a.to_string(), b.to_string()))?;
-    Some((text, Attribute::Ruby(attr)))
 }
 
 // Tokens for Rich Text.
