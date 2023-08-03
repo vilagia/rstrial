93717d7e76ec9a7a8b362de6c48ac60b001bf594
vilagia <type99hsp@gmail.com>
2023-08-04 01:46:51 +0900

    insert scene delimiter

--- rstrial_converter/src/converter.rs
+++ rstrial_converter/src/converter.rs
@@ -67,10 +67,22 @@ pub trait SectionConverter {
 pub trait ManuscriptConverter {
     type ItemConverter: SectionConverter;
     fn convert(sections: Vec<Section>) -> String {
+        let mut previous_section = None;
         sections
-            .into_iter()
-            .map(Self::ItemConverter::convert)
-            .collect::<Vec<String>>()
-            .concat()
+        .into_iter()
+        .map(|section| {
+                let section_string = match section {
+                    Section::Title(_) => format!("{}\n", Self::ItemConverter::convert(section.clone())),
+                    Section::Scene(_, _) => {
+                        match previous_section {
+                            Some(Section::Title(_)) => format!("\n{}", Self::ItemConverter::convert(section.clone())),
+                            _ => format!("\n†\n\n{}", Self::ItemConverter::convert(section.clone())),
+                        }
+                    }
+                };
+                previous_section = Some(section);
+                section_string
+            })
+            .collect()
     }
 }
