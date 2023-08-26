eb700d93b1ad8d908698a24e926b64641c726489
vilagia <type99hsp@gmail.com>
2023-08-26 16:39:03 +0000

    refactor: extract single scene check

--- rstrial_cli/src/commands/check.rs
+++ rstrial_cli/src/commands/check.rs
@@ -34,38 +34,8 @@ impl Command for CheckCommand {
             })
             .collect();
 
-        let rt: Runtime = Runtime::new().unwrap();
-
         for (doc, body) in sections {
-            let mut spinning_circle = progress::SpinningCircle::new();
-            spinning_circle.set_job_title("Waiting for LLM to finish");
-            let tags = doc.tags.join(", ");
-            let handle = rt.spawn(async move {
-                let exec: llm_chain_openai::chatgpt::Executor = executor!()?;
-                println!("{body}");
-                let temprate = r#"
-                あなたは自動化された小説制作支援システムです。以下の文章は小説の一シーンです。以下の書式に沿って著者への助言を行ってください。
-                
-                - 各タグの妥当性と、評価の理由
-                - 追加タグ案
-                - タグへの適合性を向上させる施策
-                "#;
-                let res = prompt!(
-                    temprate,
-                    "tags:{{tags}}\n\n{{body}}\n\n",
-                )
-                .run(&parameters!(
-                    "tags" => tags,
-                    "body" => body,
-                ), &exec).await.unwrap();
-                println!("{}", res);
-                Ok::<(),llm_chain::traits::ExecutorCreationError>(()) // <- note the explicit type annotation here
-            });
-            while !handle.is_finished() {
-                thread::sleep(std::time::Duration::from_millis(100));
-                spinning_circle.tick();
-            }
-            spinning_circle.jobs_done();
+            Self::check_scene(doc, body);
         }
         Ok(())
     }
@@ -76,5 +46,39 @@ impl CheckCommand {
         let content = std::fs::read_to_string(path)?;
         let tokens = ManuscriptParser::new(&content).collect();
         Ok(tokens)
+    
+    }
+
+    fn check_scene(document: Document, body: String) {
+        let rt: Runtime = Runtime::new().unwrap();
+        let mut spinning_circle = progress::SpinningCircle::new();
+        spinning_circle.set_job_title("Waiting for LLM to finish");
+        let tags = document.tags.join(", ");
+        let handle = rt.spawn(async move {
+            let exec: llm_chain_openai::chatgpt::Executor = executor!()?;
+            println!("{body}");
+            let temprate = r#"
+            あなたは自動化された小説制作支援システムです。以下の文章は小説の一シーンです。以下の書式に沿って著者への助言を行ってください。
+            
+            - 各タグの妥当性と、評価の理由
+            - 追加タグ案
+            - タグへの適合性を向上させる施策
+            "#;
+            let res = prompt!(
+                temprate,
+                "tags:{{tags}}\n\n{{body}}\n\n",
+            )
+            .run(&parameters!(
+                "tags" => tags,
+                "body" => body,
+            ), &exec).await.unwrap();
+            println!("{}", res);
+            Ok::<(),llm_chain::traits::ExecutorCreationError>(()) // <- note the explicit type annotation here
+        });
+        while !handle.is_finished() {
+            thread::sleep(std::time::Duration::from_millis(100));
+            spinning_circle.tick();
+        }
+        spinning_circle.jobs_done();
     }
 }
