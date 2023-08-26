5bad4a8b1f345209fb6206d11503ca334699ada9
vilagia <type99hsp@gmail.com>
2023-08-26 16:33:26 +0000

    refactor: more info in loop

--- rstrial_cli/src/commands/check.rs
+++ rstrial_cli/src/commands/check.rs
@@ -2,7 +2,7 @@ use std::{path::Path, thread};
 
 use llm_chain::{executor, parameters, prompt};
 use rstrial_converter::converter::{vfm::line_converter::VfmLineConverter, LineConverter};
-use rstrial_parser::{tokens::section::Section, ManuscriptParser};
+use rstrial_parser::{tokens::section::{Section, Document}, ManuscriptParser};
 use tokio::runtime::Runtime;
 
 use super::Command;
@@ -23,26 +23,25 @@ impl Command for CheckCommand {
     type Args = CheckArgs;
     fn execute(&self, args: &Self::Args) -> Result<(), Box<dyn std::error::Error>> {
         let tokens = Self::tokenize(args.target.as_path())?;
-        let sections: Vec<(Vec<String>, String)> = tokens
+        let sections: Vec<(Document, String)> = tokens
             .into_iter()
             .filter_map(|t| match t {
                 Section::Title(_title) => None,
                 Section::Scene(doc, body) => {
-                    let tags = doc.tags;
                     let body: String = body.into_iter().map(VfmLineConverter::convert).collect();
-                    Some((tags, body))
+                    Some((doc, body))
                 }
             })
             .collect();
 
         let rt: Runtime = Runtime::new().unwrap();
 
-        for (tags, body) in sections {
+        for (doc, body) in sections {
             let mut spinning_circle = progress::SpinningCircle::new();
             spinning_circle.set_job_title("Waiting for LLM to finish");
+            let tags = doc.tags.join(", ");
             let handle = rt.spawn(async move {
-                let exec = executor!()?;
-                let t: String = tags.clone().join(", ");
+                let exec: llm_chain_openai::chatgpt::Executor = executor!()?;
                 println!("{body}");
                 let temprate = r#"
                 あなたは自動化された小説制作支援システムです。以下の文章は小説の一シーンです。以下の書式に沿って著者への助言を行ってください。
@@ -56,7 +55,7 @@ impl Command for CheckCommand {
                     "tags:{{tags}}\n\n{{body}}\n\n",
                 )
                 .run(&parameters!(
-                    "tags" => t,
+                    "tags" => tags,
                     "body" => body,
                 ), &exec).await.unwrap();
                 println!("{}", res);
