70b8961049533d53bf4f8068b3fa9c6d4afe7377
vilagia <99010581+vilagia@users.noreply.github.com>
2023-08-28 02:15:54 +0900

    Refactor check subcommand implementation (#6)
    * refactor: use parameters!
    
    * refactor: prompt as multiline string
    
    * refactor: more info in loop
    
    * refactor: extract single scene check

--- rstrial_cli/src/commands/check.rs
+++ rstrial_cli/src/commands/check.rs
@@ -2,7 +2,7 @@ use std::{path::Path, thread};
 
 use llm_chain::{executor, parameters, prompt};
 use rstrial_converter::converter::{vfm::line_converter::VfmLineConverter, LineConverter};
-use rstrial_parser::{tokens::section::Section, ManuscriptParser};
+use rstrial_parser::{tokens::section::{Section, Document}, ManuscriptParser};
 use tokio::runtime::Runtime;
 
 use super::Command;
@@ -23,40 +23,19 @@ impl Command for CheckCommand {
     type Args = CheckArgs;
     fn execute(&self, args: &Self::Args) -> Result<(), Box<dyn std::error::Error>> {
         let tokens = Self::tokenize(args.target.as_path())?;
-        let sections: Vec<(Vec<String>, String)> = tokens
+        let sections: Vec<(Document, String)> = tokens
             .into_iter()
             .filter_map(|t| match t {
                 Section::Title(_title) => None,
                 Section::Scene(doc, body) => {
-                    let tags = doc.tags;
                     let body: String = body.into_iter().map(VfmLineConverter::convert).collect();
-                    Some((tags, body))
+                    Some((doc, body))
                 }
             })
             .collect();
 
-        let rt: Runtime = Runtime::new().unwrap();
-
-        for (tags, body) in sections {
-            let mut spinning_circle = progress::SpinningCircle::new();
-            spinning_circle.set_job_title("Waiting for LLM to finish");
-            let handle = rt.spawn(async move {
-                let exec = executor!()?;
-                let t: String = tags.clone().join(", ");
-                println!("{body}");
-                let res = prompt!(
-                    "あなたは自動化された小説制作支援システムです。以下の文章は小説の一シーンです。以下の書式に沿って著者への助言を行ってください。\n\n - 各タグの妥当性と、評価の理由 \n - 追加タグ案 \n - タグへの適合性を向上させる施策",
-                    format!("tags: {t}\n\n{body}\n\n").as_str(),
-                )
-                .run(&parameters!(), &exec).await.unwrap();
-                println!("{}", res);
-                Ok::<(),llm_chain::traits::ExecutorCreationError>(()) // <- note the explicit type annotation here
-            });
-            while !handle.is_finished() {
-                thread::sleep(std::time::Duration::from_millis(100));
-                spinning_circle.tick();
-            }
-            spinning_circle.jobs_done();
+        for (doc, body) in sections {
+            Self::check_scene(doc, body);
         }
         Ok(())
     }
@@ -67,5 +46,39 @@ impl CheckCommand {
         let content = std::fs::read_to_string(path)?;
         let tokens = ManuscriptParser::new(&content).collect();
         Ok(tokens)
+    
+    }
+
+    fn check_scene(document: Document, body: String) {
+        let rt: Runtime = Runtime::new().unwrap();
+        let mut spinning_circle = progress::SpinningCircle::new();
+        spinning_circle.set_job_title("Waiting for LLM to finish");
+        let tags = document.tags.join(", ");
+        let handle = rt.spawn(async move {
+            let exec: llm_chain_openai::chatgpt::Executor = executor!()?;
+            println!("{body}");
+            let temprate = r#"
+            あなたは自動化された小説制作支援システムです。以下の文章は小説の一シーンです。以下の書式に沿って著者への助言を行ってください。
+            
+            - 各タグの妥当性と、評価の理由
+            - 追加タグ案
+            - タグへの適合性を向上させる施策
+            "#;
+            let res = prompt!(
+                temprate,
+                "tags:{{tags}}\n\n{{body}}\n\n",
+            )
+            .run(&parameters!(
+                "tags" => tags,
+                "body" => body,
+            ), &exec).await.unwrap();
+            println!("{}", res);
+            Ok::<(),llm_chain::traits::ExecutorCreationError>(()) // <- note the explicit type annotation here
+        });
+        while !handle.is_finished() {
+            thread::sleep(std::time::Duration::from_millis(100));
+            spinning_circle.tick();
+        }
+        spinning_circle.jobs_done();
     }
 }
