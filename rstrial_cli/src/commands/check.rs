af4fbbb0b60ae5fbc7235da8c7b8b208d612b57d
vilagia <99010581+vilagia@users.noreply.github.com>
2023-08-28 02:15:40 +0900

    Revert "Faster response (#7)" (#10)
    This reverts commit 30cc61985e93c8d1db0619457ceaf149521305a9.

--- rstrial_cli/src/commands/check.rs
+++ rstrial_cli/src/commands/check.rs
@@ -2,12 +2,8 @@ use std::{path::Path, thread};
 
 use llm_chain::{executor, parameters, prompt};
 use rstrial_converter::converter::{vfm::line_converter::VfmLineConverter, LineConverter};
-use rstrial_parser::{
-    tokens::section::{Document, Section},
-    ManuscriptParser,
-};
-
-use tokio_stream::StreamExt;
+use rstrial_parser::{tokens::section::Section, ManuscriptParser};
+use tokio::runtime::Runtime;
 
 use super::Command;
 
@@ -27,28 +23,41 @@ impl Command for CheckCommand {
     type Args = CheckArgs;
     fn execute(&self, args: &Self::Args) -> Result<(), Box<dyn std::error::Error>> {
         let tokens = Self::tokenize(args.target.as_path())?;
-        let sections: Vec<(Document, String)> = tokens
+        let sections: Vec<(Vec<String>, String)> = tokens
             .into_iter()
             .filter_map(|t| match t {
                 Section::Title(_title) => None,
                 Section::Scene(doc, body) => {
+                    let tags = doc.tags;
                     let body: String = body.into_iter().map(VfmLineConverter::convert).collect();
-                    Some((doc, body))
+                    Some((tags, body))
                 }
             })
             .collect();
-        let rt = tokio::runtime::Builder::new_multi_thread()
-            .enable_all()
-            .build()?;
 
-        let mut spinning_circle = progress::SpinningCircle::new();
-        spinning_circle.set_job_title("Waiting for LLM to finish");
-        let handle = rt.spawn(Self::check_scenes(sections));
-        while !handle.is_finished() {
-            thread::sleep(std::time::Duration::from_millis(100));
-            spinning_circle.tick();
+        let rt: Runtime = Runtime::new().unwrap();
+
+        for (tags, body) in sections {
+            let mut spinning_circle = progress::SpinningCircle::new();
+            spinning_circle.set_job_title("Waiting for LLM to finish");
+            let handle = rt.spawn(async move {
+                let exec = executor!()?;
+                let t: String = tags.clone().join(", ");
+                println!("{body}");
+                let res = prompt!(
+                    "あなたは自動化された小説制作支援システムです。以下の文章は小説の一シーンです。以下の書式に沿って著者への助言を行ってください。\n\n - 各タグの妥当性と、評価の理由 \n - 追加タグ案 \n - タグへの適合性を向上させる施策",
+                    format!("tags: {t}\n\n{body}\n\n").as_str(),
+                )
+                .run(&parameters!(), &exec).await.unwrap();
+                println!("{}", res);
+                Ok::<(),llm_chain::traits::ExecutorCreationError>(()) // <- note the explicit type annotation here
+            });
+            while !handle.is_finished() {
+                thread::sleep(std::time::Duration::from_millis(100));
+                spinning_circle.tick();
+            }
+            spinning_circle.jobs_done();
         }
-        spinning_circle.jobs_done();
         Ok(())
     }
 }
@@ -58,55 +67,5 @@ impl CheckCommand {
         let content = std::fs::read_to_string(path)?;
         let tokens = ManuscriptParser::new(&content).collect();
         Ok(tokens)
-    }
-
-    async fn check_scenes(scenes: Vec<(Document, String)>) {
-        let handles: Vec<_> = scenes
-            .into_iter()
-            .enumerate()
-            .map(|(index, (doc, body))| Self::check_scene(index, doc, body))
-            .collect();
-        let mut results = tokio_stream::iter(handles).map(|h| h);
-        let mut res = vec![];
-        while let Some(r) = results.next().await {
-            res.push(r.await);
-        }
-        res.sort_by(|(i1, _), (i2, _)| i1.cmp(i2));
-        for (_, r) in res {
-            println!("{}", r);
-        }
-    }
-
-    async fn check_scene(index: usize, document: Document, body: String) -> (usize, String) {
-        let tags = document.tags.join(", ");
-        let exec: llm_chain_openai::chatgpt::Executor = executor!().unwrap();
-        let temprate = r#"
-            あなたは自動化された小説制作支援システムです。以下の文章は小説の一シーンです。以下の書式に沿って著者への助言を行ってください。
-            
-            - 各タグの妥当性と、評価の理由
-            - 追加タグ案
-            - タグへの適合性を向上させる施策
-            "#;
-        let res = prompt!(temprate, "tags:{{tags}}\n\n{{body}}\n\n",)
-            .run(
-                &parameters!(
-                    "tags" => tags,
-                    "body" => body,
-                ),
-                &exec,
-            )
-            .await
-            .unwrap();
-        let result = format!(
-            r#"
-## {} への分析結果
-
-{}
-
--------------
-            "#,
-            document.title, res
-        );
-        (index, result)
     }
 }
